42006c3fefd8c01fe8e6464b7c48d784083d9ef6
Faraz Khan <faraz.k89@gmail.com>
2018-12-02 09:30:11 +0000

    Add files via upload

---
+++ extract-unmapped-reads-in-batches.py
@@ -0,0 +1,35 @@
+import subprocess32
+import os
+import time
+import sys
+
+processes = set()
+max_processes = 10
+
+'''
+
+Extract unmapped reads from the bam files and convert bam to fasta
+
+'''
+
+#import a file containing the names of all the bam files.
+with open(sys.argv[1],'r') as k:
+	bam_files = k.read().splitlines()
+	
+for bam in bam_files:
+    output = bam.replace('.bam','.unmapped.bam')
+    fasta_output = output.replace('.unmapped.bam','.unmapped.fasta')    
+    cmd = 'samtools view -u -f 12 -F 256 %s > %s; samtools bam2fq %s | seqtk seq -A > %s' % (bam,output,output,fasta_output)
+    processes.add(subprocess32.Popen([cmd], shell=True))
+    print 'Submited job %s' % bam
+
+    while True:
+        jobs_running = len([p for p in processes if p.poll()==None])
+        print jobs_running
+        if jobs_running<24:break
+        os.wait()
+
+#Check if all the child processes were closed
+for p in processes:
+    if p.poll() is None:
+    	p.wait()
